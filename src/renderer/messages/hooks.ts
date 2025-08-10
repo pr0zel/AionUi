e383f18c2a189aa8b15953f92c3739c4eb465984
Ezio Lin <ninesuns.lin@outlook.com>
2025-08-11 00:35:33 +0800

    🚧 增加每个会话用户的 prompt 状态用于切换还原

--- src/renderer/messages/hooks.ts
+++ src/renderer/messages/hooks.ts
@@ -4,15 +4,22 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { createContext } from "../utils/createContext";
-import { ChatMessageStorage } from "@/common/storage";
+import { ChatMessageStorage, TChatConversation } from "@/common/storage";
 import { composeMessage, TMessage } from "@/common/chatLib";
 
 const [useMessageList, MessageListProvider, useUpdateMessageList] =
   createContext([] as TMessage[]);
 
 const [useChatKey, ChatKeyProvider, useUpdateChatKey] = createContext("");
+
+// 每个会话中 用户当前输入的尚未发送的 prompt 状态
+const [
+  useConversationPromptDraft,
+  ConversationPromptDraftProvider,
+  useUpdateConversationPromptDraft
+] = createContext<Record<TChatConversation["id"], string>>({});
 
 export const useAddOrUpdateMessage = () => {
   const update = useUpdateMessageList();
@@ -40,3 +47,28 @@ export const useMessageLstCache = (key: string) => {
 
 export { useMessageList, MessageListProvider };
 export { useChatKey, ChatKeyProvider };
+
+// #region 更新管理会话中用户的 prompt
+
+const useUpdateChatPromptDraftByKey = (key: string) => {
+  const update= useUpdateConversationPromptDraft();
+  return useCallback((prompt: string) => {
+    update((draft) => {
+      return {...draft, [key]: prompt};
+    });
+  }, [key, update]);
+}
+
+const useChatPromptDraftByKey = (key: string): string | undefined => {
+  // TODO 开启 ts 的索引严格模式
+  return useConversationPromptDraft()[key];
+}
+
+export {
+  useChatPromptDraftByKey,
+  useUpdateChatPromptDraftByKey,
+  useConversationPromptDraft,
+  ConversationPromptDraftProvider,
+};
+
+// #endregion
